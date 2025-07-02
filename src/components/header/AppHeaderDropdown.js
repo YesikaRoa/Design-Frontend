a164c38e4044a512edb9ad83b79a0cd95ce5f9c3
Yesika Roa <yroa1402@gmail.com>
2025-07-02 02:09:15 -0400

    Actualizar la fuente del avatar predeterminado en el componente AppHeaderDropdown

--- src/components/header/AppHeaderDropdown.js
+++ src/components/header/AppHeaderDropdown.js
@@ -21,6 +21,7 @@ const AppHeaderDropdown = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const avatar = useSelector((state) => state.avatar)
+  const defaultAvatar = '/avatar.png'
 
   const [tokenExpiring, setTokenExpiring] = useState(false)
   const [counter, setCounter] = useState(15)
@@ -120,7 +121,7 @@ const AppHeaderDropdown = () => {
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar || 'default-avatar.jpg'} size="md" />
+        <CAvatar src={avatar || defaultAvatar} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownItem onClick={handleProfileClick}>
